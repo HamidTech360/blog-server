622c5f041a00c7daa5fd8eda7453928b34eae145
Medaweb <68151044+HamidTech360@users.noreply.github.com>
2022-01-09 17:49:47 +0100

    Delete config directory

--- config/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = (()=>{
-    return{
-        DATABASE_URL:'mongodb://localhost/icopy',
-        JWT_SECRET:'12345',
-        PORT:3001,
-        EMAIL:'owolabihammed2001@gmail.com',
-        PASSWORD:'olalekan2019...'
-    }
-})()
